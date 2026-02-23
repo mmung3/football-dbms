0ccfc3b564f8e0aaa388433bebe79529060dd4d0
Matthew Mung <matthewmung3@gmail.com>
2026-02-23 08:26:59 -0800

    fix navbar remounting

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,21 +1,34 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 const NavigationBar = () => {
   return (
     <>
       <Navbar expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="/">BC Pro Soccer League</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">
+            BC Pro Soccer League
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/athletes">Athletes</Nav.Link>
-              <Nav.Link href="/games">Games</Nav.Link>
-              <Nav.Link href="/locator">Locator</Nav.Link>
-              <Nav.Link href="/sponsors">Sponsors</Nav.Link>
-              <Nav.Link href="/advanced">Advanced</Nav.Link>
+              <Nav.Link as={Link} to="/athletes">
+                Athletes
+              </Nav.Link>
+              <Nav.Link as={Link} to="/games">
+                Games
+              </Nav.Link>
+              <Nav.Link as={Link} to="/locator">
+                Locator
+              </Nav.Link>
+              <Nav.Link as={Link} to="/sponsors">
+                Sponsors
+              </Nav.Link>
+              <Nav.Link as={Link} to="/advanced">
+                Advanced
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
